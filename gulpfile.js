25366e78decc74a8c1dc50d518dccf813b15499b
Nicolas Hamy <nhamypro@gmail.com>
2019-03-07 16:19:47 +0100

    ajout geoffroy

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 //gulpfile.js
-var gulp = require('gulp'),
-  bundle = require('gulp-bundle-assets');
+var gulp = require('gulp');
+
 var sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const zip = require('gulp-zip');
+var gulp = require('gulp'),
+    gp_concat = require('gulp-concat'),
+    gp_uglify = require('gulp-uglify');
+cleanCSS = require('gulp-clean-css');
 
 
 // SASS, concaténation de fichiers et minification
 gulp.task('css', function () {
-  return gulp.src('*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css'));
+    return gulp.src('*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
 
 })
 
@@ -20,13 +24,25 @@ gulp.task('js', () =>
         .pipe(gulp.dest('js'))
 );
 
-gulp.task('bundle', function() {
-  return gulp.src('./bundle.config.js')
-      .pipe(bundle())
-      .pipe(gulp.dest('./public'));
+
+gulp.task('bundle_js', function(){
+    return gulp.src(['./js/*.js'])
+        .pipe(gp_concat('concat.js'))
+        .pipe(gulp.dest('public'))
+        //.pipe(gp_rename('uglify.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('public'));
 });
 
 
+gulp.task('bundle_css', function(){
+    return gulp.src(['./css/*.css'])
+        .pipe(gp_concat('concat.css'))
+        .pipe(gulp.dest('public'))
+        //.pipe(gp_rename('uglify.js'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public'));
+});
 
 
 gulp.task('zip', () =>
@@ -34,7 +50,6 @@ gulp.task('zip', () =>
         .pipe(zip('projet_gulp.zip'))
         .pipe(gulp.dest('packaging'))
 );
-
 
 const {src, task} = require('gulp');
 const eslint = require('gulp-eslint');
@@ -65,3 +80,15 @@ gulp.task('default1', function () {
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
 });
+
+
+/*
+gulp.task('installation'), function () {
+
+    gulp.task('css'),
+        gulp.task('js'),
+        gulp.task('bundle_js'),
+        gulp.task('bundle_css'),
+        gulp.task('zip')
+}
+*/
--- public/main-ba2a81b607.js
+++
@@ -1,3 +0,0 @@
-var DIV=document.getElementById("hello");DIV.innerText="Hello";
-var div=document.getElementById("world");div.innerText="world";
-//# sourceMappingURL=maps/main-ba2a81b607.js.map
