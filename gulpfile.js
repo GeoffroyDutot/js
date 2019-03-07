d3dd9bb5bf87f7c8868860aef8ebfb697e03424c
Geoffroy Dutot <dutot.geoffroy@laposte.net>
2019-03-07 15:49:22 +0100

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -34,3 +34,34 @@ gulp.task('zip', () =>
         .pipe(zip('projet_gulp.zip'))
         .pipe(gulp.dest('packaging'))
 );
+
+
+const {src, task} = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', () => {
+    return src(['scripts/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.<
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(gulp.dest('public'))
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.formatEach('compact', process.stderr))
+        .pipe(eslint.failAfterError());
+});
+
+
+'use strict';
+
+var gulp = require('gulp'),
+    sassLint = require('gulp-sass-lint');
+
+gulp.task('default1', function () {
+  return gulp.src('sass/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
